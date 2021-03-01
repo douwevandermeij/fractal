cd5885839273a6631495483458f7a2b0fb032bee
Douwe van der Meij <douwe@karibu-online.nl>
2021-03-01 17:03:12 +0100

    clean-up

--- fractal/core/command_bus/command.py
+++ fractal/core/command_bus/command.py
@@ -4,32 +4,3 @@ from dataclasses import dataclass
 @dataclass
 class Command:
     pass
-    # @property
-    # def get_account_id(self):
-    #     if hasattr(self, "auth_user"):
-    #         auth_user = getattr(self, "auth_user")
-    #         if auth_user:
-    #             return auth_user.account_id
-    #
-    # @property
-    # def user_id(self):
-    #     if hasattr(self, "auth_user"):
-    #         auth_user = getattr(self, "auth_user")
-    #         if auth_user:
-    #             return auth_user.id
-
-    # @property
-    # def get_account_id(self):
-    #     if hasattr(self, "account_id"):
-    #         return getattr(self, "account_id")
-    #     if hasattr(self, "user"):
-    #         if user := getattr(self, "user"):
-    #             return user.account_id
-    #
-    # @property
-    # def user_id(self):
-    #     if hasattr(self, "user_id"):
-    #         return getattr(self, "user_id")
-    #     if hasattr(self, "user"):
-    #         if user := getattr(self, "user"):
-    #             return user.id
