d6572375accc5b1f559871a525d675dc2fca4637
Douwe van der Meij <douwe@karibu-online.nl>
2022-08-29 14:14:19 +0200

    fix and/or specifications

--- tests/core/specifications/generic/test_collections.py
+++ tests/core/specifications/generic/test_collections.py
@@ -15,7 +15,17 @@ def test_and_specification():
     assert spec.is_satisfied_by(DC(**dict(id=1, name="a")))
 
 
-def test_specification_and():
+def test_specification_and_and():
+    spec = EqualsSpecification("id", 1).And(
+        AndSpecification([EqualsSpecification("id", 1), EqualsSpecification("name", "a")])
+    )
+    assert isinstance(spec, AndSpecification)
+    assert len(spec.specifications) == 3
+    DC = make_dataclass("DC", [("id", int), ("name", str)])
+    assert spec.is_satisfied_by(DC(**dict(id=1, name="a")))
+
+
+def test_and_specification_and():
     spec = AndSpecification([EqualsSpecification("id", 1)]).And(
         EqualsSpecification("name", "a")
     )
@@ -31,7 +41,17 @@ def test_or_specification():
     assert spec.is_satisfied_by(DC(**dict(id=1, name="b")))
 
 
-def test_specification_or():
+def test_specification_or_or():
+    spec = EqualsSpecification("id", 1).Or(
+        OrSpecification([EqualsSpecification("id", 1), EqualsSpecification("name", "a")])
+    )
+    assert isinstance(spec, OrSpecification)
+    assert len(spec.specifications) == 3
+    DC = make_dataclass("DC", [("id", int), ("name", str)])
+    assert spec.is_satisfied_by(DC(**dict(id=2, name="a")))
+
+
+def test_or_specification_or():
     spec = OrSpecification([EqualsSpecification("id", 1)]).Or(
         EqualsSpecification("name", "a")
     )
